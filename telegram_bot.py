89fe6ebcfd615227477daa01a69d1cb1435e2973
Mounika0410 <70888034+Mounika0410@users.noreply.github.com>
2020-09-10 15:40:06 +0530

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,4 +1,4 @@
-from Adafruit_IO import Client,Feed
+from Adafruit_IO import Client,Data
 import os
 x = os.getenv('x') #ADAFRUIT_IO_USERNAME
 y = os.getenv('y') #ADAFRUIT_IO_KEY
@@ -16,21 +16,24 @@ def turnoff(bot,update):
     chat_id = update.message.chat_id
     bot.send_message(chat_id,text="Light turned off")
     bot.send_photo(chat_id,photo='https://images.app.goo.gl/cU8fsGt7sSjdKS4b8')
-    from Adafruit_IO import Data
+    
 def input_message(bot,update):
    text=update.message.text
    if text == 'turn on':
       chat_id = update.message.chat_id
       bot.send_message(chat_id,text="Light turned on")
       bot.send_photo(chat_id,photo='https://images.app.goo.gl/vboPtjpGbD3iUTrf6')
-      value=Data(value=1)
-      value_send=aio.create_data('lightbot',value)
+      feed = aio.feeds('lightbot')
+      aio.send_data(feed.key,1)
+      
+      
    elif text=='turn off':
       chat_id = update.message.chat_id
       bot.send_message(chat_id,text="Light turned off")
       bot.send_photo(chat_id,photo='https://images.app.goo.gl/cU8fsGt7sSjdKS4b8')
-      value=Data(value=0)
-      value_send=aio.create_data('lightbot',value)
+      feed = aio.feeds('lightbot')
+      aio.send_data(feed.key,0)
+      
 Token=os.getenv('Token')   
 u = Updater('Token')
 dp = u.dispatcher
